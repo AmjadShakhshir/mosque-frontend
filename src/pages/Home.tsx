0e7cd76dd524ebfeb5a2c3e28d86ea6ae3645946
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-13 13:31:28 +0200

    add home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Banner from "../components/Banner";
+import Main from "../components/Main";
+
+const Home = () => {
+  return (
+    <>
+      <Main />
+      <Banner />
+    </>
+  );
+};
+
+export default Home;
