6fa71ab30ed655f6d39a3d2eb05443bb5aa88bf9
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-13 17:13:14 +0200

    change logo and add new links to nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,14 +5,19 @@ const Header = () => {
     <header>
       <Navbar expand="lg">
         <Container fluid>
-          <Navbar.Brand href="#">Mosque</Navbar.Brand>
+          <Navbar.Brand href="#">
+            <img src="/assets/logo_03.png" width="60" height="60" className="d-inline-block align-top" alt="Mosque logo" />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
               <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
+              <Nav.Link href="#action2">Prayer Times</Nav.Link>
+              <Nav.Link href="#action2">Services</Nav.Link>
+              <Nav.Link href="#action2">About</Nav.Link>
+              <Nav.Link href="#action2">Contact</Nav.Link>
             </Nav>
-            <Button variant="outline-success">Search</Button>
+            <Button className="header-btn">Donate</Button>
           </Navbar.Collapse>
         </Container>
       </Navbar>
