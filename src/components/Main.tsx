89522ed47675ad122c9eb261078c0346ef53c4d8
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-13 17:12:53 +0200

    change the title of main section

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,9 @@
 const Main = () => {
   return (
-    <main className="d-flex justify-content-between">
+    <main className="d-flex justify-content-between align-items-center">
       <section className="py-5">
-        <h1 className="fw-light">Welcome to Alrabita</h1>
-        <p className="lead text-muted">We are a team of talented developers making websites</p>
+        <h1 className="fw-bold">Suomen Islamilainen Yhdyskunta</h1>
+        <p className="lead text-muted">الرابطة الإسلامية في فنلندا</p>
       </section>
       <section className="py-5">
         <img className="main-img" src="public/assets/Mobile.png" alt="mobile" />
