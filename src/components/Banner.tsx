f5e4b6727eb7e62b10a6ed190c85ff52e44a0fb3
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-13 13:32:38 +0200

    add banner component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,9 @@
+const Banner = () => {
+  return (
+    <section className="py-5">
+      <img src="public/assets/mosquesQuran.svg" className="img-fluid rounded-top" alt="Mosques image and verses from the Quran" />
+    </section>
+  );
+};
+
+export default Banner;
