41c71af3e8f82548aaeb88089bdf6ca3dbccd57c
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-13 13:31:49 +0200

    add main component

---
+++ src/components/Main.tsx
@@ -0,0 +1,15 @@
+const Main = () => {
+  return (
+    <main className="d-flex justify-content-between">
+      <section className="py-5">
+        <h1 className="fw-light">Welcome to Alrabita</h1>
+        <p className="lead text-muted">We are a team of talented developers making websites</p>
+      </section>
+      <section className="py-5">
+        <img className="main-img" src="public/assets/Mobile.png" alt="mobile" />
+      </section>
+    </main>
+  );
+};
+
+export default Main;
