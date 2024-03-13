4595e30c6c109700f012b68268586a92038a46ce
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-13 17:12:39 +0200

    add new sections to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,18 @@
+import ArticlesSection from "../components/ArticlesSection";
 import Banner from "../components/Banner";
 import Main from "../components/Main";
+import PrayerBanner from "../components/PrayerBanner";
 
 const Home = () => {
   return (
     <>
       <Main />
+      <PrayerBanner />
       <Banner />
+      <div className="gap-3 d-flex">
+        <ArticlesSection />
+        <ArticlesSection />
+      </div>
     </>
   );
 };
