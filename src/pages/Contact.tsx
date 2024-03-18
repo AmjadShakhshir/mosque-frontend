331343e5a7d4de2a769622ea524223fdf95d79d8
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-19 00:40:15 +0200

    add contact us page

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,24 @@
+const Contact = () => {
+  return (
+    <section className="contact mt-3">
+      <div className="contact-container">
+        <h1 className="display-4 fst-italic mb-5">Contact</h1>
+        <p className="lead">
+          We are always happy to hear from you. If you have any questions or comments, please feel free to contact us. You can reach us by phone, email, or by visiting our office. Our team is here to help you with anything you need. We look forward
+          to hearing from you!
+        </p>
+        <p className="lead">
+          Office: Lönnrotinkatu 22 C 34
+          <br />
+          Mosque: Lönnrotinkatu 22 P 41
+          <br />
+          Phone: +358 50 533 5592
+          <br />
+          Email rabita(at)rabita.fi
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
