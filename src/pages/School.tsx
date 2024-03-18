b5926c7c725d419583f76eb6d8adc2141e0dfebc
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-19 00:02:33 +0200

    add more styling to school page

--- src/pages/School.tsx
+++ src/pages/School.tsx
@@ -1,20 +1,22 @@
 const School = () => {
   return (
-    <article className="school">
-      <h1>School</h1>
-      <p>
-        Our school is a place where children can learn about Islam and the Arabic language. The school is open to all children, regardless of their background or religion. We have a team of qualified teachers who are dedicated to providing a
-        high-quality education to all of our students.
-      </p>
-      <p>
-        Our school is open to children of all ages, from pre-school to high school. We offer a wide range of classes, including Quran recitation, Islamic studies, and Arabic language. Our goal is to provide a well-rounded education that will help our
-        students succeed in all areas of their lives.
-      </p>
-      <p>
-        We believe that education is the key to a better future, and we are committed to providing the best possible education to all of our students. We welcome children of all backgrounds and religions, and we are dedicated to creating a safe and
-        welcoming environment for all of our students.
-      </p>
-      <p>If you are interested in enrolling your child in our school, please contact us for more information. We look forward to welcoming you and your child to our school!</p>
+    <article className="school mt-4">
+      <div className="school-container">
+        <h1 className="display-4 fst-italic mb-5">School</h1>
+        <p className="lead">
+          Our school is a place where children can learn about Islam and the Arabic language. The school is open to all children, regardless of their background or religion. We have a team of qualified teachers who are dedicated to providing a
+          high-quality education to all of our students.
+        </p>
+        <p className="lead">
+          Our school is open to children of all ages, from pre-school to high school. We offer a wide range of classes, including Quran recitation, Islamic studies, and Arabic language. Our goal is to provide a well-rounded education that will help
+          our students succeed in all areas of their lives.
+        </p>
+        <p className="lead">
+          We believe that education is the key to a better future, and we are committed to providing the best possible education to all of our students. We welcome children of all backgrounds and religions, and we are dedicated to creating a safe and
+          welcoming environment for all of our students.
+        </p>
+        <p className="lead">If you are interested in enrolling your child in our school, please contact us for more information. We look forward to welcoming you and your child to our school!</p>
+      </div>
     </article>
   );
 };
