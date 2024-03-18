915cacec3bc403eb327a03941ef25bb0bc66b0dd
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-19 00:40:01 +0200

    add more info for the school

--- src/pages/School.tsx
+++ src/pages/School.tsx
@@ -1,21 +1,56 @@
 const School = () => {
   return (
     <article className="school mt-4">
-      <div className="school-container">
-        <h1 className="display-4 fst-italic mb-5">School</h1>
-        <p className="lead">
-          Our school is a place where children can learn about Islam and the Arabic language. The school is open to all children, regardless of their background or religion. We have a team of qualified teachers who are dedicated to providing a
-          high-quality education to all of our students.
+      <div className="school-container px-5 py-3">
+        <h1 className="display-4 fst-italic mb-5">Islamic League School</h1>
+        <p className="lead fs-6">
+          The Islamic League School in Finland was established to provide education services to Muslims residing in Finland of all categories and ages. The school aims to teach the Holy Qur’an, the Arabic language, and the principles of true Islam
+          according to a moderate curriculum that enhances belonging to Islam and spreads a culture of love and peace by increasing awareness of tolerant Islamic principles.
         </p>
-        <p className="lead">
-          Our school is open to children of all ages, from pre-school to high school. We offer a wide range of classes, including Quran recitation, Islamic studies, and Arabic language. Our goal is to provide a well-rounded education that will help
-          our students succeed in all areas of their lives.
+        <p className="lead fs-6">
+          The school witnessed several stages, but work was launched in the school according to a modern and advanced mechanism at the beginning of the 2021-2022 academic year, where a new registration mechanism and modern teaching methods were
+          adopted.
         </p>
-        <p className="lead">
-          We believe that education is the key to a better future, and we are committed to providing the best possible education to all of our students. We welcome children of all backgrounds and religions, and we are dedicated to creating a safe and
-          welcoming environment for all of our students.
+        <p className="lead fs-6">
+          The school employs qualified and experienced teachers, which helps them deliver their lofty message to the students with quality and mastery, ultimately aiming to improve the level of students and enhance their skills in reading and writing
+          in the Arabic language to achieve the ability to recite the Holy Qur’an with perfection.
         </p>
-        <p className="lead">If you are interested in enrolling your child in our school, please contact us for more information. We look forward to welcoming you and your child to our school!</p>
+        <p className="lead fs-6">
+          The school provides its services to all Muslims residing in Finland, whether from Finns or from immigrant communities of all ages, and focuses on the young age group - namely school-age students - because they are the largest group in
+          number and because they are the foundation of the future.
+        </p>
+        <div>
+          <h2 className="display-6 fst-italic mb-3">School departments:</h2>
+          <h5 className="fst-italic mb-3">1- School for children 7-17 years old </h5>
+          <p className="lead mx-5 fs-6">
+            The Holy Qur’an, the Arabic language, and Islamic culture are taught in the full academic year system from the beginning of fall to the end of spring of the following year, and vacations coincide with the general system of Finnish public
+            schools.
+          </p>
+          <h5 className="mx-5">Mechanism for implementing the study</h5>
+          <p className="lead mx-5 fs-6">A- The dual study system: lessons are held in school and remotely, where the Arabic language is taught on Saturday at school and the Holy Qur’an is taught on Sunday remotely via the Internet.</p>
+          <p className="lead mx-5 fs-6">
+            B- Distance learning system: The school is unique in providing this service among all the schools of the Holy Qur’an and Arabic language in Finland, which has led to the benefit of Muslim children residing in remote cities and remote
+            villages that do not have a large Muslim community that allows lessons to be held there. To register for the school, click here
+          </p>
+          <p className="lead mx-5 fs-6">
+            To register for the school, <a href="#">click here</a>
+          </p>
+        </div>
+        <div>
+          <h5 className="fst-italic mb-3">2- Arabic language courses for non-native speakers</h5>
+          <p className="lead mx-5 fs-6">
+            These courses aim to teach students reading and writing skills in Arabic up to the intermediate level B1, as well as learning the principles of Tajweed, enabling them to master reading the Qur’an and understanding its meanings.
+          </p>
+          <p className="lead mx-5 fs-6">
+            To register for the course <a href="#">click here</a>
+          </p>
+        </div>
+        <div>
+          <h5 className="fst-italic mb-3">Contact Information</h5>
+          <p className="lead mx-5 fs-6">Address: Lönnrotinkatu 22 00120 Helsinki</p>
+          <p className="lead mx-5 fs-6">Email: madrasah(a)rabita.fi</p>
+          <p className="lead mx-5 fs-6">Phone: +358 46 740 6622</p>
+        </div>
       </div>
     </article>
   );
